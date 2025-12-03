b0ccc64bfd177babd42f62bca93d660dce506afd
Matthis Boreaniz <matthisonepiece@gmail.com>
2025-12-03 11:25:47 +0100

    add routes for goals

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const recipeRoutes = require('./routes/recipes');
 const cuisineRoutes = require('./routes/cuisines');
 const ingredientRoutes = require('./routes/ingredients');
 const userRoutes = require('./routes/users');
+const goalRoutes = require('./routes/goals');
 
 // Import middleware
 const errorHandler = require('./middleware/errorHandler');
@@ -41,7 +42,7 @@ app.use('/api/recipes', recipeRoutes);
 app.use('/api/cuisines', cuisineRoutes);
 app.use('/api/ingredients', ingredientRoutes);
 app.use('/api/users', userRoutes);
-
+app.use('/api/goals', goalRoutes);
 // Home route
 app.get('/', (req, res) => {
     res.json({
